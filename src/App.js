72b7f78aa137e1022c6cc2a141a2e3c071c5a315
Cho, Yongjoon <w2000wkd@gmail.com>
2018-04-20 15:15:23 +0900

    Add ages and months

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Grid, Header, Menu } from 'semantic-ui-react'
+import range from 'lodash/range'
+
 import WeekMatrix from './components/WeekMatrix'
 import Config from './components/Config'
 
@@ -8,7 +10,7 @@ import './App.css'
 class App extends Component {
   state = {
     profile: {
-      dob: '2018/04/19',
+      dob: '2018-04-19',
       lifeExpectancy: 80
     }
   }
@@ -21,8 +23,27 @@ class App extends Component {
 
   render() {
     const { profile } = this.state
-    const lifeExpectancy = 80
-    const cells = new Array(lifeExpectancy * 52)
+    const { dob, lifeExpectancy } = profile
+
+    const numRows = Math.min(lifeExpectancy, 200)
+    let cells = []
+    if (!isNaN(new Date(dob).getTime())) {
+      cells = range(numRows * numWeeks).map(index => {
+        // TODO: change this date to correct years
+        const start = new Date(dob)
+        start.setDate(start.getDate() + index * numDays)
+        const end = new Date(dob)
+        end.setDate(end.getDate() + (index + 1) * numDays - 1)
+
+        return {
+          index,
+          title: `${getDateString(start)} ~ ${getDateString(end)} (${index +
+            1})`,
+          start,
+          end
+        }
+      })
+    }
 
     return (
       <div className="App">
@@ -50,3 +71,10 @@ class App extends Component {
 }
 
 export default App
+
+const getDateString = date => {
+  return date.toISOString().slice(0, 10)
+}
+
+const numWeeks = 52
+const numDays = 7
--- src/components/WeekMatrix.js
+++ src/components/WeekMatrix.js
@@ -8,6 +8,40 @@ class WeekMatrix extends Component {
     cells: PropTypes.array.isRequired
   }
 
+  renderMonth = ({ index, cellIndex }) => {
+    const x = cellIndex * cellWidth
+    const y = 10
+
+    return (
+      <text
+        key={cellIndex}
+        transform={`translate(${x},${y})`}
+        fontSize="14"
+        fontFamily="Lato"
+        textAnchor="start"
+      >
+        {index + 1}
+      </text>
+    )
+  }
+
+  renderAge = age => {
+    const x = 24
+    const y = (age + 1) * cellHeight
+
+    return (
+      <text
+        key={age}
+        transform={`translate(${x},${y})`}
+        fontSize="14"
+        fontFamily="Lato"
+        textAnchor="end"
+      >
+        {age}
+      </text>
+    )
+  }
+
   renderWeekCell = (i, j) => {
     const { cells } = this.props
     const cellIndex = i * numWeeks + j
@@ -15,21 +49,17 @@ class WeekMatrix extends Component {
     const style = Object.assign({}, cell.style, defaultCellStyle)
     const { stroke, fill } = style
 
-    const width = 100 / numWeeks
-    const height = 1
-
     return (
-      <g key={cellIndex}>
-        <rect
-          x={`${j * width}%`}
-          y={`${i * height}%`}
-          width={`${width}%`}
-          height={`${height}%`}
-          stroke={stroke}
-          fill={fill}
-          data-tip={cell ? cell.title : ''}
-        />
-      </g>
+      <rect
+        key={cellIndex}
+        x={j * cellWidth}
+        y={i * cellHeight}
+        height={cellHeight}
+        width={cellWidth}
+        stroke={stroke}
+        fill={fill}
+        data-tip={cell ? cell.title : ''}
+      />
     )
   }
 
@@ -43,15 +73,43 @@ class WeekMatrix extends Component {
     const { cells } = this.props
     const numRows = Math.ceil(cells.length / numWeeks)
 
+    const ages = range(Math.ceil(numRows / ageDistance)).map(
+      x => x * ageDistance
+    )
+
+    const months = []
+    if (cells.length >= 52) {
+      let currentMonth = null
+      for (let i = 0; i < 52; i++) {
+        const month = cells[i].start.getMonth()
+        if (currentMonth !== month) {
+          currentMonth = month
+          months.push({ index: month, cellIndex: i })
+        }
+      }
+    }
+
     const rows = range(numRows - 1).map(i => {
       return this.renderWeekRow(i)
     })
-    const finalRow = this.renderWeekRow(numRows - 1, cells.length % numWeeks)
+    const finalRow = this.renderWeekRow(
+      numRows - 1,
+      cells.length - (numRows - 1) * numWeeks
+    )
+
+    const height = numRows * cellHeight
+    const width = numWeeks * cellWidth
 
     return (
       <div>
-        <svg height={svgHeight} width={svgWidth + margin}>
-          <g>
+        <svg height={height + marginTop} width={width + marginLeft}>
+          <g transform={`translate(${marginLeft}, 0)`}>
+            {months.map(this.renderMonth)}
+          </g>
+          <g transform={`translate(0, ${marginTop})`}>
+            {ages.map(this.renderAge)}
+          </g>
+          <g transform={`translate(${marginLeft}, ${marginTop})`}>
             {rows}
             {finalRow}
           </g>
@@ -65,11 +123,14 @@ class WeekMatrix extends Component {
 export default WeekMatrix
 const numWeeks = 52
 
+const marginTop = 30
+const marginLeft = 30
+
+const ageDistance = 5
+
+const cellWidth = 9
+const cellHeight = 9
 const defaultCellStyle = {
   stroke: '#ffffff',
   fill: '#eeeeee'
 }
-
-const svgHeight = 1000
-const svgWidth = 500
-const margin = 30
